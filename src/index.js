69ca144b54d393034df0e613e22740c45f977546
Todea Iulia <iulia21ana@gmail.com>
2024-04-17 11:06:34 -0900

    add js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./style.css";
+document.addEventListener("DOMContentLoaded", function () {
+  var menuIcon = document.querySelector(".menu-icon");
+  var menu = document.querySelector(".menu");
+
+  menuIcon.addEventListener("click", function () {
+    if (menu.style.display == "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var adopt = document.querySelector(".adopt");
+  adopt.addEventListener("click", function () {
+    if (adopt.innerText === "Adopt don't shop") {
+      adopt.innerText = "Thank you for adopting!";
+    } else {
+      adopt.innerText = "Adopt don't shop";
+    }
+  });
+});
